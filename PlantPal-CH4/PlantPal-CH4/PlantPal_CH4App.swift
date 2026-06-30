e5eb0a3421858e40c6a0f783fd33213dc6b43564
nathanyongg <nathanjyong37@gmail.com>
2026-06-30 14:52:29 +0800

    Project

---
+++ PlantPal-CH4/PlantPal-CH4/PlantPal_CH4App.swift
@@ -0,0 +1,17 @@
+//
+//  PlantPal_CH4App.swift
+//  PlantPal-CH4
+//
+//  Created by Nathan Yong on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PlantPal_CH4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
